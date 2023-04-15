b41a84ae24c3c36c96c5948c06350d93b3551c8b
Pedro Javier Lora <javierpd45@gmail.com>
2023-04-15 00:18:03 -0400

    Se creo carton de bingo que se genera automaticamente y cambios en la interfaz

---
+++ BingoStars75/Inicio.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BingoStars75
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Cyan;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(412, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar Cartón";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnGenerarCarton_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aquamarine;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(817, 457);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 63);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SALIR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnSALIR_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Cyan;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(412, 400);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 63);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Continuar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btnContinuar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Yellow;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(271, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(418, 157);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(488, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Genera tu cartón y empieza a ganar!";
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(938, 543);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bingo Stars 75";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BingoStars75/Inicio.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BingoStars75
+{
+    public partial class Inicio : Form
+    {
+
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSALIR_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGenerarCarton_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+
+            for (int i = 0; i < 5; i++)
+            {
+                DataGridViewColumn columna1 = new DataGridViewColumn(new DataGridViewTextBoxCell());
+                columna1.Name = (i + 1).ToString();
+                columna1.Width = 75;
+                dataGridView1.Columns.Add(columna1);
+            }
+            dataGridView1.Rows.Add(5);
+
+            dataGridView1[0, 0].Value = "B";
+            dataGridView1[1, 0].Value = "I";
+            dataGridView1[2, 0].Value = "N";
+            dataGridView1[3, 0].Value = "G";
+            dataGridView1[4, 0].Value = "O";
+
+            Random random = new Random();
+            int[,] matrizRandom = new int[5,6];
+            int numeroRandom;
+            List<int> numerosGenerados = new List<int>();
+
+            for (int filas = 0; filas < 5; filas++)
+            {
+                for (int columnas = 1; columnas < 6; columnas++)
+                {
+                    switch (filas)
+                    {
+                        case 0:
+
+                            do
+                            {
+                                numeroRandom = random.Next(1, 16);
+                            } while (numerosGenerados.Contains(numeroRandom));
+
+                            matrizRandom[filas, columnas] = numeroRandom;
+                            numerosGenerados.Add(numeroRandom);
+                            
+                            break;
+
+                        case 1:
+
+                            do
+                            {
+                                numeroRandom = random.Next(16, 31);
+                            } while (numerosGenerados.Contains(numeroRandom));
+
+                            matrizRandom[filas, columnas] = numeroRandom;
+                            numerosGenerados.Add(numeroRandom);
+
+                            break;
+
+                        case 2:
+
+                            do
+                            {
+                                numeroRandom = random.Next(31, 46);
+                            } while (numerosGenerados.Contains(numeroRandom));
+
+                            matrizRandom[filas, columnas] = numeroRandom;
+                            numerosGenerados.Add(numeroRandom);
+
+                            break;
+
+                        case 3:
+
+                            do
+                            {
+                                numeroRandom = random.Next(46, 61);
+                            } while (numerosGenerados.Contains(numeroRandom));
+
+                            matrizRandom[filas, columnas] = numeroRandom;
+                            numerosGenerados.Add(numeroRandom);
+
+                            break;
+
+                        case 4:
+
+                            do
+                            {
+                                numeroRandom = random.Next(61, 76);
+                            } while (numerosGenerados.Contains(numeroRandom));
+
+                            matrizRandom[filas, columnas] = numeroRandom;
+                            numerosGenerados.Add(numeroRandom);
+
+                            break;
+                    }
+                }
+            }
+
+            for (int filas = 0; filas < 5; filas++)
+            {
+                for(int columnas = 1; columnas < 6; columnas++)
+                {
+                    dataGridView1[filas, columnas].Value = matrizRandom[filas, columnas];
+                }
+            }
+        }
+
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+
+        }        
+    }
+}
